e6906c90f0b31603f3ac43f88b2dee2ae20455c8
saasbook <saasbook@saasbook.(none)>
2012-03-19 10:12:18 -0700

    hw3-1

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,9 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+      
+    when / home page$/
+      '/'  
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
